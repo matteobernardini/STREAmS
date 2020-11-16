c27803fa1ec6cad85ecc17415697b0b3e9e8ae4f
Matteo Bernardini <matteo.bernardini@uniroma1.it>
2020-11-16 13:10:51 +0100

    added twall

--- src/mod_streams.F90
+++ src/mod_streams.F90
@@ -60,7 +60,7 @@ module mod_streams
  real(mykind), parameter :: ggmopr = gamma*gm/pr
  real(mykind), parameter :: vtexp  = 3._mykind/4._mykind
  real(mykind), parameter :: rfac   = 0.89_mykind !pr**(1._mykind/3._mykind)
- real(mykind) :: rm,re,taw,sqgmr,s2tinf,retauinflow,trat
+ real(mykind) :: rm,re,taw,sqgmr,s2tinf,retauinflow,trat,twall
  real(mykind) :: rtrms
  real(mykind), dimension(0:nsolmax) :: tsol, tsol_restart
  real(mykind) :: dtsave, dtsave_restart
